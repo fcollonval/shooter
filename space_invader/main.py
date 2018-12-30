c154e91bdb0bbd513187c399a34345522183aa03
Frederic Collonval <fcollonval@gmail.com>
2018-12-30 15:48:08 +0100

    Rework the configuration settings

--- space_invader/main.py
+++ space_invader/main.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-
-from kivy.app import App
-from kivy.clock import Clock
 from kivy.config import Config
 
-from engine import ShooterGame
-
+# Config.set should be used before importing any other Kivy modules
+# src: https://kivy.org/doc/stable/api-kivy.config.html
 WIDTH = 800
 HEIGHT = 600
 Config.set("graphics", "width", WIDTH)
 Config.set("graphics", "height", HEIGHT)
-Config.write()
+Config.set("kivy", "keyboard_mode", "system")
+
+from kivy.app import App
+from kivy.clock import Clock
+
+from engine import ShooterGame
 
 
 class ShooterApp(App):
     def build(self):
-        global WIDTH
-        global HEIGHT
         game = ShooterGame(WIDTH, HEIGHT)
         Clock.schedule_interval(game.game_update, 1.0 / 60.0)
         return game
